d1cb40892a98817c1f0f5dbbee9e8672b2a8f76c
Raxuis <raclot.raphael@gmail.com>
2025-02-19 21:42:40 +0100

    style(header): improve header button ui

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,8 +2,10 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import {Button} from "@/components/ui/button";
 import {Layout} from "@/components/Layout";
+import MagneticButton from "@/components/Button/MagneticButton";
+import {Button} from "@/components/ui/button";
+import {Sparkles} from "lucide-react";
 
 export default function Header() {
 
@@ -17,9 +19,30 @@ export default function Header() {
                            className="rounded-lg shadow-md"/>
                     Livre Track
                 </Link>
-                <Button variant="outline" className="text-gray-900 border-gray-300 hover:bg-gray-100">
-                    Se connecter
-                </Button>
+                <MagneticButton>
+                    <Button
+                        variant="outline"
+                        asChild
+                        className="relative overflow-hidden font-inter group px-6 py-4 rounded-xl border border-gray-300 shadow-md transition-all duration-300
+        bg-white/30 backdrop-blur-md hover:shadow-lg hover:border-amber-500"
+                    >
+                        <Link href="/book-shelf" className="flex items-center gap-2 font-semibold text-lg">
+                            <span
+                                className="relative z-10 text-gray-900 transition-colors duration-300 group-hover:text-amber-600">
+                                Commencer l&#39;aventure
+                            </span>
+                            <Sparkles
+                                className="relative z-10 -me-1 ms-2 opacity-70 transition-transform duration-300 group-hover:scale-110 group-hover:text-amber-600"
+                                size={18}
+                                strokeWidth={2}
+                                aria-hidden="true"
+                            />
+                            <span
+                                className="absolute inset-0 bg-gradient-to-r from-amber-500 to-yellow-400 opacity-0 transition-opacity duration-500 group-hover:opacity-20"></span>
+                        </Link>
+                    </Button>
+                </MagneticButton>
+
             </Layout>
         </header>
     );
