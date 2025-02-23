ed373c478968538cf30f0e8ed79895021494e7eb
Raxuis <raclot.raphael@gmail.com>
2025-02-23 11:23:41 +0100

    feat(dashboard): add dashboard page with metadatas

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import type {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: "Livre Track Dashboard",
+    description: "Le tableau de bord de Livre Track",
+};
+
+const MyComponent = () => {
+    return (
+        <div>
+            Dashboard
+        </div>
+    );
+};
+
+export default MyComponent;
