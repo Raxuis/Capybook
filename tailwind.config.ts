ac06fd477276c6b9455809b8db3a8499e0b29a83
Raxuis <raclot.raphael@gmail.com>
2025-04-16 22:50:00 +0200

    feat(badge): add BadgeNotification component with confetti effect and auto-close functionality

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -88,11 +88,16 @@ export default {
                     to: {
                         height: '0'
                     }
-                }
+                },
+                "shrink": {
+                    from: {width: "100%"},
+                    to: {width: "0%"},
+                },
             },
             animation: {
                 'accordion-down': 'accordion-down 0.2s ease-out',
-                'accordion-up': 'accordion-up 0.2s ease-out'
+                'accordion-up': 'accordion-up 0.2s ease-out',
+                "shrink": "shrink linear forwards",
             }
         }
     },
