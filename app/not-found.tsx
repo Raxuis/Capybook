dbf681463d0ad61cb4f69f42ab5acea44c63eea0
Raxuis <raclot.raphael@gmail.com>
2025-05-01 16:17:04 +0200

    refactor(middleware, not-found): enhance route protection and add custom 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function NotFound() {
+    return (
+        <div className="md:mt-40 flex flex-col items-center justify-center bg-background">
+            <Image src="/not-found.webp" alt="Page non trouvée" width={300} height={300}
+                   className="w-1/2 md:w-[300px]"/>
+
+            <h1 className="text-4xl font-bold text-primary mb-4">404</h1>
+            <h2 className="text-2xl text-muted-foreground mb-8">Page non trouvée</h2>
+            <p className="text-muted-foreground mb-4 text-center max-w-md max-md:px-2">
+                Désolé, la page que vous recherchez n'existe pas ou a été déplacée.
+            </p>
+            <Link
+                href="/"
+                className="px-4 py-2 bg-primary text-primary-foreground rounded-md hover:bg-primary/90 transition-colors"
+            >
+                Retour à l'accueil
+            </Link>
+        </div>
+    );
+}
