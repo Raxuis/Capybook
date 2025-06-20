e85e20e2f8ee73c7acc1709918f82adbfd2a5753
Raxuis <raclot.raphael@gmail.com>
2025-06-20 14:19:23 +0200

    feat(validation): update feedback field to be optional and trim whitespace

--- utils/zod.ts
+++ utils/zod.ts
@@ -30,9 +30,11 @@ export const ReviewBookSchema = object({
     rating: string()
         .min(1, {message: "La note est nécessaire"})
         .regex(/^[1-5]$/, "La note doit être entre 1 et 5"),
-    feedback: string()
-        .min(1, {message: "Le commentaire est nécessaire"})
-        .max(500, {message: "Le commentaire ne peut pas dépasser 500 caractères"}),
+    feedback: z
+        .string()
+        .max(500, "Le commentaire ne peut pas dépasser 500 caractères.")
+        .optional()
+        .transform((val) => val?.trim() || undefined)
 });
 
 export const PageNumberSchema = object({
