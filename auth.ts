4bcf2975de17ac47a8d1e59490849fca58a5b8bb
Raxuis <raclot.raphael@gmail.com>
2025-09-17 21:04:13 +0200

    refactor(auth): rename NEXT_AUTH_SECRET to NEXTAUTH_SECRET for consistency

--- auth.ts
+++ auth.ts
@@ -8,7 +8,7 @@ import bcrypt from "bcryptjs";
 
 export const {handlers, signIn, auth} = NextAuth({
     adapter: PrismaAdapter(prisma),
-    secret: process.env.NEXT_AUTH_SECRET,
+    secret: process.env.NEXTAUTH_SECRET,
     session: {
         strategy: "jwt",
         maxAge: 30 * 24 * 60 * 60,
