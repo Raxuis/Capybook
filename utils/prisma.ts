81ade75eb85e018027127c7259fbfcf7c39fdb2e
Raxuis <raclot.raphael@gmail.com>
2025-11-28 14:00:27 +0100

    fix(build): fix build errors

--- utils/prisma.ts
+++ utils/prisma.ts
@@ -1,12 +1,14 @@
 import {neonConfig, Pool} from '@neondatabase/serverless'
-import { PrismaNeon } from '@prisma/adapter-neon'
-import { PrismaClient } from '@prisma/client'
+import {PrismaNeon} from '@prisma/adapter-neon'
+import {PrismaClient} from '@prisma/client'
 import ws from 'ws'
 
-const connectionString = process.env.DATABASE_URL as string
-neonConfig.webSocketConstructor = ws;
-const pool = new Pool({ connectionString })
-const adapter = new PrismaNeon(pool)
-const prisma = new PrismaClient({ adapter, log: ['query', 'info', 'warn', 'error'] })
+const connectionString = process.env.DATABASE_URL!
+
+neonConfig.webSocketConstructor = ws as any;
+
+const pool = new Pool({connectionString})
+const adapter = new PrismaNeon(pool as any)
+const prisma = new PrismaClient({adapter})
 
 export default prisma
